fe3ba27bbc7dc5117685ae7d05df1bf626fc8fd8
ssstryx <jepsiejoyberon@gmail.com>
2025-12-02 10:11:29 -0600

    Created BNS Modules

--- README.md
+++
@@ -1,1 +0,0 @@
-# BarangayProject
--- Services/IEmailSender.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace BarangayProject.Services
-{
-    public interface IEmailSender
-    {
-        Task SendEmailAsync(string toEmail, string subject, string htmlMessage);
-    }
-}
